896eb328ea6138e6190a1ffa22cf41806767287d
raf-xtgt <rafaquat9905@gmail.com>
2024-04-22 08:28:46 +0600

    Spring boot backend setup

---
+++ backend/insightink/src/main/java/com/api/insightink/InsightinkApplication.java
@@ -0,0 +1,13 @@
+package com.api.insightink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightinkApplication.class, args);
+	}
+
+}
---
+++ backend/insightink/src/test/java/com/api/insightink/InsightinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.insightink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
